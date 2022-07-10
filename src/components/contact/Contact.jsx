cbb53c88aa0ac93679cefa0f52baf6224c8703a6
WarrenQuan <quanwarren11@gmail.com>
2022-07-10 09:34:20 -0700

    Experimenting with scrolling

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -7,7 +7,7 @@ export default function Contact() {
   return (
     <div className="contact" id="contact">
       <div className="wrapper">
-        <h1><strong>if(youWantToContactMe == true) &#123;</strong></h1>
+        <h1><strong>if(you<wbr/>Want<wbr/>To<wbr/>Contact<wbr/>Me == true) &#123;</strong></h1>
         <h2>
           feel free to email me at <a href='mailto:wquan@princeton.edu'><Mail className='icon' />wquan@princeton.edu</a> or message me on  <a href='https://www.linkedin.com/in/warren-quan/'><LinkedIn className='icon' /> LinkedIn</a>! i am always interested in new opportunities to grow and am happy to answer any questions! <Typewriter options={{ strings: ['thank you so much for coming!', 'come back soon!', ':D'], autoStart: true, delay: 75, loop: true, skipAddStyles: false }} />
         </h2>
